e25e00c3ac75bf405159a1f1399698683cecb9f7
erickramosxp <erickramossilva@gmail.com>
2024-02-27 18:12:39 -0300

    close windows

--- main.c
+++ main.c
@@ -23,8 +23,15 @@ int	on_keypress(int key, t_data *mlx)
 	printf("%d", key);
 	if (key == XK_Escape)
 		mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
-	if (key == 17)
-		mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
+	return (0);
+}
+
+int destroy_window(t_data *mlx)
+{
+	mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
+	mlx_destroy_display(mlx->mlx_ptr);
+	free(mlx->mlx_ptr);
+	exit(0);
 	return (0);
 }
 
@@ -35,8 +42,8 @@ int	main(void)
 	int height;
 	int width;
 
-	height = 128;
-	width = 96;
+	height = 256;
+	width = 128;
 
 	mlx.mlx_ptr = mlx_init();
 	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, 640, 480, "window");
@@ -47,11 +54,12 @@ int	main(void)
 
 	mlx_loop_hook(mlx.mlx_ptr, &handle_no_event, &mlx);
 	mlx_hook(mlx.win_ptr, KeyPress, KeyPressMask, &on_keypress, &mlx);
-	mlx_hook(mlx.win_ptr, 17, 1L, &on_keypress, &mlx);
+	mlx_hook(mlx.win_ptr, 17, 0, &destroy_window, &mlx);
 
 	mlx_destroy_image(mlx.mlx_ptr, img);
 
 	mlx_loop(mlx.mlx_ptr);
+	
 	mlx_destroy_display(mlx.mlx_ptr);
 
 	free(mlx.mlx_ptr);
