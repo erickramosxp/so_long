6593062d019bb7fe7555d1e8a48f774d261a018d
erramos <erickramossilva@outlook.com>
2024-03-01 18:23:02 -0300

    add move sprite

--- main.c
+++ main.c
@@ -10,9 +10,11 @@
 
 typedef struct s_collet
 {
-	int current;
+	int qtd_collect;
 	void *collect[6];
 	void *current_collect;
+	int *cord_x;
+	int *cord_y;
 } t_collet;
 
 typedef struct s_data
@@ -27,7 +29,7 @@ typedef struct s_data
 	t_collet collect;
 	void *floor;
 	void *wall;
-	void *door;
+	void *door[2];
 }	t_data;
 
 void	destroy_img(t_data mlx)
@@ -48,7 +50,8 @@ void	destroy_img(t_data mlx)
 	}
 	mlx_destroy_image(mlx.mlx_ptr, mlx.floor);
 	mlx_destroy_image(mlx.mlx_ptr, mlx.wall);
-	mlx_destroy_image(mlx.mlx_ptr, mlx.door);
+	mlx_destroy_image(mlx.mlx_ptr, mlx.door[0]);
+	mlx_destroy_image(mlx.mlx_ptr, mlx.door[1]);
 	i = 0;
 	while (mlx.map[i])
 	{
@@ -64,6 +67,8 @@ int	destroy_window(t_data *mlx)
 	mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
 	mlx_destroy_display(mlx->mlx_ptr);
 	free(mlx->mlx_ptr);
+	free(mlx->collect.cord_x);
+	free(mlx->collect.cord_y);
 	exit(0);
 	return (0);
 }
@@ -82,26 +87,6 @@ int	on_keypress(int key, t_data *mlx)
 
 	if (key == XK_Escape)
 	{
-		/*
-		int	i;
-
-		i = 0;
-		while (i < 4)
-		{
-			mlx_destroy_image(mlx->mlx_ptr, mlx->player[i]);
-			i++;
-		}
-		i = 0;
-		while (i < 6)
-		{
-			mlx_destroy_image(mlx->mlx_ptr, mlx->collect.collect[i]);
-			i++;
-		}
-		mlx_destroy_image(mlx->mlx_ptr, mlx->wall);
-		mlx_destroy_image(mlx->mlx_ptr, mlx->door);
-		mlx_destroy_image(mlx->mlx_ptr, mlx->floor);
-		*/
-	//	destroy_img(*mlx);
 		destroy_window(mlx);
 	}
 	else if (key == XK_Up && mlx->map[mlx->player_y - 1][mlx->player_x] != '1')
@@ -157,7 +142,7 @@ void put_game(t_data mlx)
 			else if (mlx.map[y][x] == 'C')
 				mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.collect.current_collect,  x * 32, y * 48);
 			else if (mlx.map[y][x] == 'E')
-				mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.door,  x * 32, y * 48);
+				mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.door[0],  x * 32, y * 48);
 			else 
 				mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.floor, x * 32, y * 48);
 			x++;
@@ -166,38 +151,25 @@ void put_game(t_data mlx)
 	}
 }
 
-void	animation_collect(t_data mlx)
+void	animation_collect(t_data **mlx)
 {
-	static int current;
+	static int i;
+	static int frames;
 
-	if (current == 0)
+	if (frames == 50)
 	{
-		mlx.collect.current_collect = mlx.collect.collect[0];
-		printf("%d\n", current);
+		(*mlx)->collect.current_collect = (*mlx)->collect.collect[i];
+		i++;
+		if (i == 5)
+			i = 0;
+		frames = 0;
 	}
-	else if (current == 1 * 1000)
-	{
-		mlx.collect.current_collect = mlx.collect.collect[1];
-	}
-	else if (current == 2 * 1000)
-	{
-		mlx.collect.current_collect = mlx.collect.collect[2];
-	}
-	else if (current == 3 * 1000)
-	{
-		mlx.collect.current_collect = mlx.collect.collect[3];
-	}
-	else if (current == 4 * 1000)
-	{
-		mlx.collect.current_collect = mlx.collect.collect[4];
-		current = 0;
-	}
-	current += 1;
+	frames++;
 }
 
 int put_init(t_data *mlx)
 {
-	animation_collect(*mlx);
+	animation_collect(&mlx);
 	put_game(*mlx);
 	return (1);
 }
@@ -237,6 +209,7 @@ int	main(int argc, char **argv)
 		line = get_next_line(fd);
 		i++;
 	}
+	//int c_count = 0;
 	free(line);
 	mlx.map[i] = '\0';
 	y = 0;
@@ -250,6 +223,9 @@ int	main(int argc, char **argv)
 				mlx.player_x = x;
 				mlx.player_y = y;
 			}
+
+			// int * c_y 40 c_y[0] => 40
+			// int * c_x 20 c_x[0] => 20
 			if (mlx.map[y][x] == '\n')
 				mlx.map[y][x] = '\0';
 			x++;
@@ -259,12 +235,46 @@ int	main(int argc, char **argv)
 	close(fd);
 	height = 48;
 	width = 32;
-	mlx.collect.current = 0;
+
+	mlx.collect.qtd_collect = 0;
 
 	mlx.mlx_ptr = mlx_init();
-	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, x * 32, y * 48, "window");
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, (x - 1) * 32, y * 48, "window");
+	y = 0;
+	while (mlx.map[y])
+	{
+		x = 0;
+		while (mlx.map[y][x])
+		{
+			if (mlx.map[y][x] == 'C')
+			{
+				mlx.collect.qtd_collect++;
+			}
+			x++;
+		}
+		y++;
+	}
+	mlx.collect.cord_x = malloc(sizeof(int) * mlx.collect.qtd_collect);
+	mlx.collect.cord_y = malloc(sizeof(int) * mlx.collect.qtd_collect);
 
-	mlx.collect.current = 0;
+	y = 0;
+	i = 0;
+	while (mlx.map[y])
+	{
+		x = 0;
+		while (mlx.map[y][x])
+		{
+			if (mlx.map[y][x] == 'C')
+			{
+				mlx.collect.cord_x[i] = x;
+				mlx.collect.cord_y[i] = y;
+				i++;
+			}
+			x++;
+		}
+		y++;
+	}
+
 	mlx.collect.collect[0] = mlx_xpm_file_to_image(mlx.mlx_ptr, "./Wizard stay/magic1.xpm", &height, &width);
 	mlx.collect.collect[1] = mlx_xpm_file_to_image(mlx.mlx_ptr, "./Wizard stay/magic2.xpm", &height, &width);
 	mlx.collect.collect[2] = mlx_xpm_file_to_image(mlx.mlx_ptr, "./Wizard stay/magic3.xpm", &height, &width);
@@ -273,7 +283,8 @@ int	main(int argc, char **argv)
 	mlx.collect.collect[5] = mlx_xpm_file_to_image(mlx.mlx_ptr, "./Wizard stay/magic6.xpm", &height, &width);
 
 
-	mlx.door = mlx_xpm_file_to_image(mlx.mlx_ptr,"./Wizard stay/door-close.xpm", &height, &width);
+	mlx.door[0] = mlx_xpm_file_to_image(mlx.mlx_ptr,"./Wizard stay/door-open.xpm", &height, &width);
+	mlx.door[1] = mlx_xpm_file_to_image(mlx.mlx_ptr,"./Wizard stay/door-close.xpm", &height, &width);
 	mlx.wall = mlx_xpm_file_to_image(mlx.mlx_ptr,"./Wizard stay/wall.xpm", &height, &width);
 	mlx.floor = mlx_xpm_file_to_image(mlx.mlx_ptr,"./Wizard stay/floor.xpm", &height, &width);
 	mlx.player[0] = mlx_xpm_file_to_image(mlx.mlx_ptr, "./Wizard stay/player_front.xpm", &height, &width);
@@ -291,41 +302,9 @@ int	main(int argc, char **argv)
 	mlx_loop(mlx.mlx_ptr);
 
 	mlx_destroy_display(mlx.mlx_ptr);
-/*
-*/
+
+
 	free(mlx.mlx_ptr);
-
-	
-/*	i = 0;
-	while (i < 4)
-	{
-		mlx_destroy_image(mlx.mlx_ptr, mlx.player[i]);
-		i++;
-	}
-	i = 0;
-	while (i < 6)
-	{
-		mlx_destroy_image(mlx.mlx_ptr, mlx.collect.collect[i]);
-		i++;
-	}
-	mlx_destroy_image(mlx.mlx_ptr, mlx.floor);
-	mlx_destroy_image(mlx.mlx_ptr, mlx.wall);
-
-
-
-	mlx_loop(mlx.mlx_ptr);
-	
-	mlx_destroy_display(mlx.mlx_ptr);
-
-	i = 0;
-	while (mlx.map[i])
-	{
-		free(mlx.map[i]);
-		i++;
-	}
-	free(mlx.map);
-
-	free(mlx.mlx_ptr);*/
 	return (0);
 }
 
